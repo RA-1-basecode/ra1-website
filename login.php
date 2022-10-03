c85baa552b7541153765a132eaa9f8dbc0f04f7c
uniquecoded <ferlanferlani.id@gmail.com>
2022-10-03 20:34:54 +0700

    push

---
+++ login.php
@@ -0,0 +1,77 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8" />
+    <meta content="width=device-width, initial-scale=1.0" name="viewport" />
+
+    <title>Ra 1 Teknik Informatika - Login</title>
+    <meta content="" name="description" />
+    <meta content="" name="keywords" />
+
+    <!-- Favicons -->
+    <link href="assets/img/favicon.png" rel="icon" />
+    <link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon" />
+
+    <!-- Google Fonts -->
+    <link
+      href="https://fonts.googleapis.com/css?family=Poppins:300,400,500,600,700"
+      rel="stylesheet"
+    />
+
+    <!-- Vendor CSS Files -->
+    <link href="assets/vendor/animate.css/animate.min.css" rel="stylesheet" />
+    <link
+      href="assets/vendor/bootstrap/css/bootstrap.min.css"
+      rel="stylesheet"
+    />
+    <link
+      href="assets/vendor/bootstrap-icons/bootstrap-icons.css"
+      rel="stylesheet"
+    />
+    <link href="assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet" />
+
+    <!-- Template Main CSS File -->
+    <link href="assets/css/style.css" rel="stylesheet" />
+  </head>
+
+  <body>
+
+
+  <!-- welcoem card login -->
+  <div class="container-fluid">
+  <div class="row justify-content-center mt-5">
+    <div class="col-sm-6">
+    <div class="welcome-login text-center">
+    <h4>Selamat datang silahkan login</h4>
+    </div>
+       <div class="card shadow-lg">
+        <div class="card-body">
+        <form>
+		    <div class="mb-3">
+		   <label class="form-label">Username</label>
+			<input class="form-control form-control-md" type="email" name="email" placeholder="Username Anda" />
+		    </div>
+			<div class="mb-3">
+		    <label class="form-label">Password</label>
+			<input class="form-control form-control-md" type="password" name="password" placeholder="Password Anda" />
+		</div>
+		<div>
+          </label>
+			</div>
+		<button type="submit" class="btn btn-primary">Masuk</button>
+		</form>
+        </div>
+       </div>
+    </div>
+  </div>
+  </div>
+  <!-- end welcome card login -->
+    <!-- Vendor JS Files -->
+    <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <script src="assets/vendor/swiper/swiper-bundle.min.js"></script>
+    <script src="assets/vendor/php-email-form/validate.js"></script>
+
+    <!-- Template Main JS File -->
+    <script src="assets/js/main.js"></script>
+  </body>
+</html>
